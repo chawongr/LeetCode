4cca3caf6e792db134c6e713f28c7d3fe484d97a
Chanidapa Wongtep <96721815+chawongr@users.noreply.github.com>
2023-01-07 22:45:03 +0700

    Easy

--- Palindrome_9.py
+++ Palindrome_9.py
@@ -1,11 +1,7 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         x = str(x)
-        if len(x)==1:
-            return True
-        elif len(x)==2 and x[0]==x[-1]:
-            return True
-        elif x[0]==x[-1]:
-            return self.isPalindrome(x[1:-1])
-        else:
-            return False
+        if len(x)==1:return True
+        elif len(x)==2 and x[0]==x[-1]:return True
+        elif x[0]==x[-1]:return self.isPalindrome(x[1:-1])
+        else:return False

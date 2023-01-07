f8c6ae8dde6b6afe6c5defff3e021f6e02de1352
Chanidapa Wongtep <96721815+chawongr@users.noreply.github.com>
2023-01-08 03:24:50 +0700

    Medium

--- ReverseInteger_7.py
+++ ReverseInteger_7.py
@@ -1,15 +1,13 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        rev,temp=[],[]
+        rev,temp,ans=[],[],''
         x=str(x)
         for i in x:
             if i=='-':temp.append(i)
             else:rev.append(int(i))
         rev=list(reversed(rev))
-        while len(rev)!=0:
-            temp.append(rev.pop(0))
-        ans=''
+        while len(rev)!=0:temp.append(rev.pop(0))
         for i in temp:
             ans+=str(i)
-        if int(ans)>=-2147483648 and int(ans)<=2147483647:return int(ans)
+        if int(ans)>=-1*pow(2,31) and int(ans)<=pow(2,31)-1:return int(ans)
         else:return 0

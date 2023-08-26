d1c05473283c702839c216120b01c0e14b150f8d
Chanidapa Wongtep <96721815+chawongr@users.noreply.github.com>
2023-08-27 00:18:48 +0700

    Easy

--- 9_Palindrome.py
+++ 9_Palindrome.py
@@ -1,7 +1,7 @@
-class Solution:
-    def isPalindrome(self, x: int) -> bool:
+class Solution(object):
+    def isPalindrome(object,  x):
         x = str(x)
         if len(x)==1:return True
         elif len(x)==2 and x[0]==x[-1]:return True
-        elif x[0]==x[-1]:return self.isPalindrome(x[1:-1])
+        elif x[0]==x[-1]:return Solution.isPalindrome(object,x[1:-1])
         else:return False
